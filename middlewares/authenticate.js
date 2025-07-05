a1deae030ab5d85106848723052737c6d9eaf864
Ruslan Isupov <111610909+Ruslan-Isupov@users.noreply.github.com>
2025-07-05 23:41:08 +0300

    ADDED: auth operations

---
+++ middlewares/authenticate.js
@@ -0,0 +1,22 @@
+import HttpError from "../helpers/HttpError.js";
+
+import { findUser } from "../services/authServices.js";
+
+import { verifyToken } from "../helpers/jwt.js";
+
+const authenticate = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization)
+    return next(HttpError(401, "Authorization header missing"));
+  const [bearer, token] = authorization.split(" ");
+  if (bearer !== "Bearer")
+    return next(HttpError(401, "Authorization header not Bearer"));
+
+  const { payload, error } = verifyToken(token);
+  if (error) return next(HttpError(401, error.message));
+  const user = await findUser({ id: payload.id });
+  if (!user || !user.token) return next(HttpError(401, "User not found"));
+  req.user = user;
+  next();
+};
+export default authenticate;
